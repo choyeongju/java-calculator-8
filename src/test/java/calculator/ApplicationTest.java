491ad473365a5344ddcb4cdcb7a9d356b913d0f5
choyeongju <jjudy1301@naver.com>
2025-10-17 00:10:28 +0900

    test: 커스텀 구분자 처리 테스트 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -32,6 +32,62 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Test
+    void 커스텀_단일문자_구분자() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2;3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_영어_다글자_구분자() {
+        assertSimpleTest(() -> {
+            run("//av\\n1av2av3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_한글_다글자_구분자() {
+        assertSimpleTest(() -> {
+            run("//ㅇㄴ\\n1ㅇㄴ2ㅇㄴ3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_정규식특수문자_구분자_별표() {
+        assertSimpleTest(() -> {
+            run("//*\\n1*2*3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_정규식특수문자_구분자_파이프() {
+        assertSimpleTest(() -> {
+            run("//|\\n1|2|3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_정규식특수문자_구분자_물음표() {
+        assertSimpleTest(() -> {
+            run("//?\\n1?2?3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_단일값() {
+        assertSimpleTest(() -> {
+            run("//;\\n1");
+            assertThat(output()).contains("결과 : 1");
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
