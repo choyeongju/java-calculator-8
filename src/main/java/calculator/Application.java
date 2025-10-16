0d6efba88274f96335779df6037aef16d86ab085
choyeongju <jjudy1301@naver.com>
2025-10-16 23:59:25 +0900

    feat: 기본 문자열 덧셈 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,28 @@
 package calculator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String inputLine = reader.readLine();
+
+            String expression = inputLine;
+            String delimiterPattern = "[,:]";
+            String[] tokens = expression.split(delimiterPattern);
+            int total = 0;
+
+            for (String token : tokens) {
+                total += Integer.parseInt(token);
+            }
+
+            System.out.println("결과 : " + total);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
